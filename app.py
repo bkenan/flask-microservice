e899248530ef205d7e6fa29b942399f964e02318
Kanan Bakirli <kanan.bk@gmail.com>
2022-04-12 18:33:28 -0400

    updated

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def index():
     return render_template('index.html', the_title='My Portfolio Website')
 
 @app.route('/ds.html')
-def myth():
+def ds():
     return render_template('web_dev.html', the_title='Data Science')
 
 @app.route('/web_dev.html')
-def symbol():
+def wd():
     return render_template('ds.html', the_title='Web Development')
 
 
